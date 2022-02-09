8cd96c017b5c63c25361516b9730bd832f51fbe8
AlanAnibalDSR <alananibaldsr@gmail.com>
2022-02-08 18:27:57 -0800

    Lesson 3.15

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,25 @@
-window.alert("This is an alert! JavaScript is running!")
+// Storing User Input Using Variables//
+//window.prompt("What is your robot's name?");//
+
+//capture and store that response - Intro Var //
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName + " is ready for battle!");
+
+
+//
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+//Introducting Functions //
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+// Calling a Function //
+//fight();
